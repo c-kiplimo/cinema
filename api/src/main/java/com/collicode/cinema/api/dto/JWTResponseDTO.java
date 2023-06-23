b122b7a3704354ccd9adc5b0b2cf6e909020bc1e
c-kiplimo <kiplimocollins855@gmail.com>
2023-06-23 20:59:53 +0300

    dto:JWTResponseDto,LoginDto,UserDto

---
+++ api/src/main/java/com/collicode/cinema/api/dto/JWTResponseDTO.java
@@ -0,0 +1,25 @@
+package com.collicode.cinema.api.dto;
+
+public class JWTResponseDTO {
+	
+	public String token;
+	
+	public JWTResponseDTO() {
+		
+	}
+
+	public JWTResponseDTO(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+
+}
